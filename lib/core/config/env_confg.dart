b5c4062f05e0a7b186fe58031b84a75f5ca134af
Ispx <isaquespx98@gmail.com>
2023-11-05 17:24:55 -0300

    refactor

---
+++ lib/core/config/env_confg.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'package:martinlog_web/extensions/string_extension.dart';
+
+abstract interface class IEnvConfig {
+  Future<void> read(String path);
+}
+
+final class EnvConfig implements IEnvConfig {
+  static final EnvConfig _i = EnvConfig._();
+  EnvConfig._();
+  factory EnvConfig() => _i;
+  String get urlBase => _getValue<String>("URL_BASE");
+  String get enviroment => _getValue<String>("ENVIROMENT");
+
+  var _data = {};
+  T _getValue<T>(String key) => _data[key].toString().parseToType<T>();
+  @override
+  Future<void> read(String? env) async {
+    try {
+      String? path = env == "PROD" ? ".env_prod" : ".env_dev";
+      final fileEnv = File(path);
+      var rows = await fileEnv.readAsLines();
+      _data = {for (var row in rows) row.split("=").first: row.split("=").last};
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
