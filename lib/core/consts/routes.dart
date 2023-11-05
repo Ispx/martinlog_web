2c3e4b6af69679cc9a1ad9006de97299f7dac22a
Ispx <isaquespx98@gmail.com>
2023-11-05 17:25:02 -0300

    created path routes

---
+++ lib/core/consts/routes.dart
@@ -0,0 +1,9 @@
+class Routes {
+  static final Routes _i = Routes._();
+  Routes._();
+  factory Routes() => _i;
+  static const auth = "/login";
+  static const company = "/company";
+  static const dock = "/dock";
+  static const operation = "/operation";
+}
