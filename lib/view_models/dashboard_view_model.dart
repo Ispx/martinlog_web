54419b05d98fc345860ce890f4815961c27781ea
Ispx <isaquespx98@gmail.com>
2025-05-25 18:41:34 -0300

    refactor dashboard fetch

--- lib/view_models/dashboard_view_model.dart
+++ lib/view_models/dashboard_view_model.dart
@@ -1,14 +1,11 @@
 import 'package:get/get.dart';
-import 'package:martinlog_web/models/company_model.dart';
 import 'package:martinlog_web/models/dashboard_model.dart';
-import 'package:martinlog_web/models/dock_model.dart';
 import 'package:martinlog_web/models/operation_model.dart';
 import 'package:martinlog_web/repositories/dashboard_repository.dart';
 import 'package:martinlog_web/repositories/get_operations_repository.dart';
 import 'package:martinlog_web/state/app_state.dart';
 
 abstract interface class IDashboardViewModel {
-  Future<void> fetchLastestOperations();
   Future<void> fetchDashboard();
   DashboardModel? getDashboard({
     required int idDockType,
@@ -33,22 +30,14 @@ final class DashboardViewModel extends GetxController
     this.appState.value = appState;
   }
 
-  @override
-  Future<void> fetchLastestOperations() async {
-    try {
-      changeState(AppStateLoading());
-      operations.value = await getOperationsRepository(limit: 5);
-      changeState(AppStateDone());
-    } catch (e) {
-      changeState(AppStateError(e.toString()));
-    }
-  }
+
 
   @override
   Future<void> fetchDashboard() async {
     try {
       changeState(AppStateLoading());
       dashboardResults.value = await dashboardRepository();
+      operations.value = await getOperationsRepository(limit: 5);
       changeState(AppStateDone());
     } catch (e) {
       changeState(AppStateError(e.toString()));
