e97dcd7e54a77ac140c63b3c380bd4660c7b41e0
Ispx <isaquespx98@gmail.com>
2025-05-26 18:49:46 -0300

    fix search filter

--- lib/views/web/users_view.dart
+++ lib/views/web/users_view.dart
@@ -107,6 +107,7 @@ class _UserViewState extends State<UserView> {
                     label: 'Pesquisar',
                     hint: 'Pesquise por nome, documento ou transportadora',
                     onChange: (e) => textSearched.value = e,
+                    maxLength: 1,
                   ),
                 ),
                 SizedBox(
