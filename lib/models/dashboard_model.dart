71a2ad091cfa2ffa3ed774c5bf8d292f5d78ede1
Ispx <isaquespx98@gmail.com>
2025-07-11 21:38:31 -0300

    added propertie name in dashboard

--- lib/models/dashboard_model.dart
+++ lib/models/dashboard_model.dart
@@ -3,12 +3,14 @@ class DashboardModel {
   final int today;
   final int inProgress;
   final int idDockTpe;
+  final String name;
 
   DashboardModel({
     required this.total,
     required this.inProgress,
     required this.today,
     required this.idDockTpe,
+    required this.name,
   });
   factory DashboardModel.fromJson(Map map) {
     return DashboardModel(
@@ -16,6 +18,7 @@ class DashboardModel {
       total: map['total'],
       today: map['today'],
       inProgress: map['inProgress'],
+      name: map['name'],
     );
   }
 }
